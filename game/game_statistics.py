cf16b070522754f7bf7896100b4463f462b673b0
SykkoAtHome <rafal@tluchowski.p>
2023-05-25 16:50:48 +0200

    test

--- game/game_statistics.py
+++ game/game_statistics.py
@@ -1,10 +1,7 @@
 
 
-
-
-
-def user_calculate_distance():
+def user_calculate_distance(user_id, game_id):
     pass
 
-def user_respawn_count():
+def user_respawn_count(user_id, game_id):
     pass
--- userData/userData.py
+++ userData/userData.py
@@ -1,14 +1,8 @@
-import os
-import sys
-sys.path.append("../db")
 from db import c, conn
-
-
-
 from datetime import datetime
 
 
-def gather_user_locations(user_id, game_id):
+def user_location_data(user_id, game_id):
     userCheck = c.execute(f"SELECT exists (SELECT * from location_events WHERE user_id = {user_id}) AS extists;")
     userCheck = c.fetchall()
     gameCheck = c.execute(f"SELECT exists (SELECT * from location_events WHERE user_id = {game_id}) AS extists;")
@@ -17,10 +11,10 @@ def gather_user_locations(user_id, game_id):
     user_locations = {}
 
     for x in userCheck:
-        userExists = x[0]
+        userExists = int(x[0])
 
     for y in gameCheck:
-        gameExists = y[0]
+        gameExists = int(y[0])
 
     if userExists and gameExists:
         locationdb = c.execute("SELECT latitude, longitude, event_time from location_events WHERE user_id = 1 AND game_id = 1;")
@@ -41,36 +35,55 @@ def gather_user_locations(user_id, game_id):
         for x in range(len(locationData)):
             game_list.append(game_id)
             user_locations["game_id"] = game_list
+        conn.close()
+        return user_locations  # dictionary
     else:
         conn.close()
-        print("Either user_id or game_id does not exist.")
+        return "Either user_id or game_id does not exist."
 
-    return user_locations
+
 
 def get_user_name(user_id):
 
     sql = f"SELECT username FROM users WHERE user_id={user_id}"
     c.execute(sql)
     dane = c.fetchall()
+    conn.close()
     for tuple in dane:
         for username in tuple:
+
             return str(username)
 
 
 def get_display_name(user_id):
-
     sql = f"SELECT display_name FROM users WHERE user_id={user_id}"
     c.execute(sql)
     dane = c.fetchall()
+    conn.close()
     for tuple in dane:
         for displayname in tuple:
+
             return str(displayname)
+
+
+def get_user_last_location(user_id, game_id):
+    sql = f"SELECT latitude, longitude, event_time from location_events WHERE user_id = {user_id} AND game_id = {game_id} ORDER BY event_time DESC LIMIT 1;"
+    c.execute(sql)
+    dane = c.fetchall()
+    conn.close()
+    last_location = []
+    for x in dane:
+        last_location.append(float(x[0]))
+        last_location.append(float(x[1]))
+        last_location.append(str(x[2]))
+
+    return last_location  # list
 
 
 def set_user_location(user_id, game_id, pos_x, pos_y):
     time = datetime.now()
     sql = f"INSERT INTO location_events (user_id, game_id, event_time, latitude, longitude, event_type) VALUES ({user_id}, {game_id}, '{time}', {pos_x}, {pos_y}, 6);"
     c.execute(sql)
+    conn.close()
 
-
-#print(get_user_name(1))
+print(user_location_data(2,1))
