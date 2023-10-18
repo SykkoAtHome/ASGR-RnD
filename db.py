ee1ef6924eb4dd65958e9c7e426b0d5f2dbd58e7
Rafał Tłuchowski <rafal@tluchowski.pl>
2023-10-18 16:37:33 +0200

    update

--- db.py
+++
@@ -1,9 +0,0 @@
-import pymysql
-
-dbHost = "192.168.0.20"
-dbName = "asgr"
-dbUsername = "python"
-dbPass = "P^^th0n312"
-
-conn = pymysql.connect(host=dbHost, user=dbUsername, password=dbPass, database=dbName, charset="utf8", autocommit=True)
-c = conn.cursor()
--- game/game_statistics.py
+++
@@ -1,7 +0,0 @@
-
-
-def user_calculate_distance(user_id, game_id):
-    pass
-
-def user_respawn_count(user_id, game_id):
-    pass
